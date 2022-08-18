ea9fde453af525b0f0c9ed3f88f3a8596cbf8929
Renzo Vinci <renzvinci@gmail.com>
2022-08-17 22:50:48 -0300

    after class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import { getPokemones } from "./data/pokemones";
-import Container from './components/Container';
+import Vegeta from './components/Vegeta';
+import VegetaF from './components/VegetaF';
 
 function App() {
-  const pokemones = getPokemones()
   return (
-      <Container data={pokemones} title="Tengo que atraparlos!"/>
-  );
+    <>
+      <Vegeta />
+      <VegetaF />
+    </>
+  )
 }
 
 export default App;
